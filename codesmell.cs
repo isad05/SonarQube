405def53aca839c03df67b4f2fd5868e24c249ae
isad05 <38412396+isad05@users.noreply.github.com>
2023-12-04 10:10:44 +0800

    Create codesmell.cs

---
+++ codesmell.cs
@@ -0,0 +1,15 @@
+public class CodeSmellExample {
+    public void DoSomething() {
+        // A long method that does multiple things
+        // ...
+
+        // Nested if statements
+        if (condition1) {
+            if (condition2) {
+                // Code block
+            }
+        }
+
+        // ...
+    }
+}
