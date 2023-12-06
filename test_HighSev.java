db6e54fa3d492357296a6844f68dbde8e17f76bf
isad05 <38412396+isad05@users.noreply.github.com>
2023-12-06 14:01:38 +0800

    Update test_HighSev.java

--- test_HighSev.java
+++ test_HighSev.java
@@ -1,20 +1,19 @@
-public class HighSeverityExample {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class HighSeverityExample {
 
     public void unhandledException() {
         try {
-
             throw new RuntimeException("High Severity: Unhandled exception");
         } catch (Exception e) {
-
         }
     }
 
-    // High Severity Issue: Hardcoded password
     public String getPassword() {
         return "HighSeverity@123";
     }
-
 
     public void deprecatedMethod() {
         String deprecatedValue = "Deprecated method: High Severity";
@@ -29,7 +28,13 @@ public class HighSeverityExample {
 
     public void sqlInjectionVulnerability(String userInput) {
         String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
+    }
 
+    public void resourceLeak() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/database", "user", "password");
+        } catch (SQLException e) {
+        }
     }
 
     public static void main(String[] args) {
@@ -39,5 +44,6 @@ public class HighSeverityExample {
         example.deprecatedMethod();
         example.infiniteLoop();
         example.sqlInjectionVulnerability("user'); DROP TABLE users; --");
+        example.resourceLeak();
     }
 }
