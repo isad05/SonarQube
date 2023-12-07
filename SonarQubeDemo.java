3e18ac7518046a21432d361fd7795686eae6d61d
isad05 <38412396+isad05@users.noreply.github.com>
2023-12-07 10:45:45 +0800

    Create SonarQubeDemo.java

---
+++ SonarQubeDemo.java
@@ -0,0 +1,12 @@
+public class SonarQubeDemo {
+
+    public void demonstrateSonarQubeIssue() {
+        String password = "password";
+        System.out.println("Password: " + password);
+    }
+
+    public static void main(String[] args) {
+        SonarQubeDemo demo = new SonarQubeDemo();
+        demo.demonstrateSonarQubeIssue();
+    }
+}
