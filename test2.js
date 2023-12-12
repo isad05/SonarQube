09af586c458207c78bacbf52e46c24bf536088cc
isad05 <38412396+isad05@users.noreply.github.com>
2023-12-12 13:52:14 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,16 +1,1 @@
-function unsafeEvalExample(userInput) {
-    let result;
-  
-    // Potential security risk: Using eval with user input
-    eval('result = ' + userInput);
-
-      totalPrice = price * quantity;
-  
-    return totalPrice;
-  }
-  
-  const userInput = prompt('Enter a value:'); // Simulating user input
-  
-  console.log('Result:', unsafeEvalExample(userInput));
-
 
